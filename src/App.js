e8f4297e0c4ed61d0aa0545e3b7db25811af5377
betsystevens <betsystevens5@gmail.com>
2021-03-15 22:03:44 -0500

    fetch from php crud api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+
+const url = 'http://localhost/php-crud-api/api.php';
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch(function(err) {
+    console.error(`error: ${err}`);
+    console.error(`oops, network error`);
+    console.error(`(it's not you, it's them)`);
+  });
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <header className='App-header'>Hello World</header>
     </div>
   );
 }
